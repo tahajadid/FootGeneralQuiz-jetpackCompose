3a0a316fda2259b7cc121cf192a064a28a91da49
Taha Jadid <jadid.taha@waroom.com>
2023-06-19 16:02:18 +0100

    Add next button in the viewModel

--- app/src/main/java/com/example/footgeneralquiz/MainActivity.kt
+++ app/src/main/java/com/example/footgeneralquiz/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.footgeneralquiz
 
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.footgeneralquiz.navigation.Navigation
 import com.example.footgeneralquiz.theme.FootGeneralQuizTheme
 
